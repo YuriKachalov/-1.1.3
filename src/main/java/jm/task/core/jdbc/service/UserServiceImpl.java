e2ac719d6c8c8c2197f7bc71aab8f90e8618ceeb
Yuri Kachalov <kachalovjuri28@yandex.ru>
2025-03-03 19:05:38 +0300

    Исправлено замечания от ментора

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,35 +1,35 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl us = new UserDaoJDBCImpl();
+    private final UserDao US = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        us.createUsersTable();
+        US.createUsersTable();
     }
 
     public void dropUsersTable() {
-        us.dropUsersTable();
+        US.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        us.saveUser(name, lastName, age);
+        US.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        us.removeUserById(id);
+        US.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-
-        return us.getAllUsers();
+        return US.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        us.cleanUsersTable();
+        US.cleanUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,8 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/basa";
@@ -11,7 +13,7 @@ public class Util {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            if (!connection.isClosed()){
+            if (!connection.isClosed()) {
                 return connection;
             }
         } catch (SQLException e) {
